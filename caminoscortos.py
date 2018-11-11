fe083b54d46f0b656d400c8530f04fb8e11b8c7a
Santiago <s.saenz11@uniandes.edu.co>
2018-11-10 21:36:58 -0500

    Algoritmo de Floyd wheather funcionando

--- caminoscortos.py
+++ caminoscortos.py
@@ -1,6 +1,7 @@
+from collections import deque
 import numpy as np
 import networkx as nx
-input = np.loadtxt("tarea6\distances5.txt", dtype='i' )
+input = np.loadtxt('/Users/Santiago/Documents/Dalgo/Tarea6Dalgo/distances5.txt', dtype='i' )
 graph = nx.DiGraph()
 nodes = input.shape[1]
 
@@ -21,4 +22,47 @@ for row in input:
         else:
             j+=1
 
-print(graph.edges)
+
+
+# Implementación de algoritmo de Dijkstra
+
+def dijkstra(graph, initial):
+    visited = {initial: 0}
+    path = {}
+
+    nodes = set(graph.nodes)
+    print(nodes)
+
+    while nodes:
+        min_node = None
+        for node in nodes:
+            if node in visited:
+                if min_node is None:
+                    min_node = node
+                elif visited[node] < visited[min_node]:
+                    min_node = node
+        if min_node is None:
+            break
+
+        nodes.remove(min_node)
+        current_weight = visited[min_node]
+
+        for edge in graph.edges(min_node):
+            try:
+                weight = current_weight + graph.distances[(min_node, edge)]
+            except:
+                continue
+            if edge not in visited or weight < visited[edge]:
+                visited[edge] = weight
+                path[edge] = min_node
+
+    return visited, path
+
+print("Resultado del algoritmo de dijkstra")    
+print(dijkstra(graph, 0))    
+
+
+  
+
+#Implementación de 
+
